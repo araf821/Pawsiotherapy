6aa62bb9c8e673844bd0e5b704482a6a699c167f
araf821 <araf.ahmed200@gmail.com>
2023-05-06 16:52:26 -0400

    implemented card component

--- app/find/animals/AnimalCard.tsx
+++ app/find/animals/AnimalCard.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { SafeAnimal } from "@/app/types";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface AnimalCardProps {
   count: number;
@@ -6,11 +9,45 @@ interface AnimalCardProps {
 }
 
 const AnimalCard: React.FC<AnimalCardProps> = ({ count, animal }) => {
-  console.log(animal);
+  const router = useRouter();
 
   return (
-    <div className="relative w-full rounded-2xl border-4 border-black">
-      <div className="m-1 h-20 rounded-lg bg-yellow-500">{animal.name}</div>
+    <div className="rounded-lg bg-white capitalize shadow-md transition duration-300 hover:shadow-2xl">
+      {/* Image Container */}
+      <div className="relative mx-auto flex aspect-square w-full flex-col items-center justify-center overflow-hidden rounded-t-lg">
+        <Image
+          alt="animal card"
+          src={animal.image}
+          fill
+          className="h-full w-full overflow-hidden object-cover"
+        />
+      </div>
+      {/* Info */}
+      <div className="mt-1 flex flex-col gap-1 p-2">
+        <div className="flex items-center justify-between text-sm font-light text-neutral-800">
+          <p>{animal.species}</p>
+          <p>{animal.location}</p>
+        </div>
+        <p className="text-2xl font-semibold">{animal.name}</p>
+        <p>
+          <span className="bg-zinc-800 px-2 text-white">
+            {animal.personality}
+          </span>
+        </p>
+        <p>
+          <span className="bg-zinc-800 px-2 text-white">
+            {animal.age} Years Old
+          </span>
+        </p>
+        <button
+          onClick={() => {
+            router.push(`/animal/${animal.id}`);
+          }}
+          className="my-1 w-full rounded-md border-2 border-zinc-800 p-2 transition duration-300 hover:bg-zinc-800 hover:text-white"
+        >
+          View More
+        </button>
+      </div>
     </div>
   );
 };
